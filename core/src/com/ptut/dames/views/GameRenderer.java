61af7cb5d757c27495e6bb6965a484f7ea6f86bd
John <lynnjonathanp@gmail.com>
2016-12-27 08:14:27 -0800

    Add feedback
    I added some comments on the GameRenderer.

--- core/src/com/ptut/dames/views/GameRenderer.java
+++ core/src/com/ptut/dames/views/GameRenderer.java
@@ -16,7 +16,7 @@ import com.ptut.dames.model.Plateau;
 
 public class GameRenderer implements Renderer {
 
-    private final Stage stage = new Stage(new FitViewport(10, 10));
+    private final Stage stage = new Stage(new FitViewport(10, 10)); // I recommend initializing this stage in your constructor the same way you do for stage2.
     private Table hud;
     InputMultiplexer multiplexer;
     protected ControlPlateau controller;
@@ -30,17 +30,17 @@ public class GameRenderer implements Renderer {
         
         
          
-        Gdx.input.setInputProcessor(this.stage);
-        this.stage.addActor(plateau);
-        this.controller = controller;
-         multiplexer= new InputMultiplexer();
-         camera=new OrthographicCamera();
-        stage2 = new Stage();
+        Gdx.input.setInputProcessor(this.stage); // Replace this line with your multiplexer code in render(); you want to set the multiplexer as your input processor.
+        this.stage.addActor(plateau);           // Good.
+        this.controller = controller;           // Good.
+         multiplexer= new InputMultiplexer();   // Good.
+         camera=new OrthographicCamera();       // Good.
+        stage2 = new Stage();                   // Good.
         
-        skinneon = new Skin(Gdx.files.internal("neonui/neonui/neon-ui.json" ));
-         menu = new TextButton("menu", skinneon);
-         menu.setPosition(10,10);
-         stage2.addActor(menu);
+        skinneon = new Skin(Gdx.files.internal("neonui/neonui/neon-ui.json" )); // Good.
+         menu = new TextButton("menu", skinneon);                               // Good.
+         menu.setPosition(10,10);               // Good.
+         stage2.addActor(menu);                 // Good.
          
          
        
@@ -54,18 +54,18 @@ public class GameRenderer implements Renderer {
 
     @Override
     public void render(float delta) {
-        Plateau plateau = new Plateau();
+        Plateau plateau = new Plateau(); // As a general rule of thumb, you do not want to create new classes in your render method. Move this to the constructor.
        
-        Gdx.gl.glClearColor(.0f, .3f, .4f, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-        this.stage.draw();
-        multiplexer.addProcessor(stage);
-        multiplexer.addProcessor(new GestureDetector(new ControlPlateau(plateau)));
-        multiplexer.addProcessor(stage2);
+        Gdx.gl.glClearColor(.0f, .3f, .4f, 1);      // Good.
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);   // Good.
+        this.stage.draw();                          // Good, but you should also call "this.stage.act()" like you do for stage2
+        multiplexer.addProcessor(stage);            // This belongs in the constructor; you don't want it to reset every frame in render().
+        multiplexer.addProcessor(new GestureDetector(new ControlPlateau(plateau))); // Same as above.
+        multiplexer.addProcessor(stage2);           // Same as above.
         
-        Gdx.input.setInputProcessor(multiplexer);
-        stage2.act();
-        stage2.draw();
+        Gdx.input.setInputProcessor(multiplexer);   // Same as above.
+        stage2.act();                               // Good.
+        stage2.draw();                              // Good.
     }
 
     @Override
